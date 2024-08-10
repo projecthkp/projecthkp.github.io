1afb85c99e7697b8e75812ca5cf137a78d11e756
Alakok77 <bosskamon1105@gmail.com>
2024-08-10 12:36:06 +0700

    BOSS

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Example: Functionality for a responsive menu toggle
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
-
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
